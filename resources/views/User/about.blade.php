fb662bc542bc43cf572282014fc6cba6e65b8769
Abood <abdallahabukaraki10@gmail.com>
2022-03-23 19:17:34 +0300

    edit about us

--- resources/views/User/about.blade.php
+++ resources/views/User/about.blade.php
@@ -5,13 +5,13 @@
     <section class="about-section section-ptb">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 order-sm-last order-lg-first pr-xl-5">
+                <div class="col-lg-8 order-sm-last order-xs-last order-lg-first pr-xl-5">
                     <div class="about-content mb-4 mb-lg-0 pr-lg-5">
                         <h3>{{$about->title}}</h3>
                         <p style="line-height:30px">{!!$about->description!!}</p>
                     </div>
                 </div>
-                <div class="col-lg-4 order-sm-first order-lg-last" style="margin-bottom: 30px">
+                <div class="col-lg-4 order-sm-first order-xs-first order-lg-last" style="margin-bottom: 30px">
                     <div class="about-image">
                         <img src="{{$about->getMedia('about')[0]->getUrl()}}" alt="about image">
                     </div>
