6dc6c68875bb7f4190b7087660784b0949b4b009
Abood <abdallahabukaraki10@gmail.com>
2022-03-26 20:19:05 +0300

    edit response

--- resources/views/User/SubBranche/index.blade.php
+++ resources/views/User/SubBranche/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-6 col-md-3">
                     <div class="product-item">
                         <div class="product-thumb">
-                            <img src="{{$subBranche->getMedia('branch')->first()->getUrl()}}" style="height: 250px; width: 250px " alt="product">
+                            <img src="{{$subBranche->getMedia('branch')->first()->getUrl()}}" alt="product">
                         </div>
                         <div class="product-content">
                             <h6><a href="/sub-branches-products/{{$subBranche->id}}/{{$crops->id}}" class="product-title">{{$subBranche->name}}</a></h6>
