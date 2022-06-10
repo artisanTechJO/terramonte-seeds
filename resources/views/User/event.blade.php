ad1abb19723a5c5bf02777bb69819aa598686365
yazeed nazal <yazeednazal96@gmail.com>
2022-06-10 23:01:18 +0300

    video fixes part 9

--- resources/views/User/event.blade.php
+++ resources/views/User/event.blade.php
@@ -35,15 +35,26 @@
     </style>
 @endsection
 @section('content')
-<!-- about section start -->
+    <!-- about section start -->
     <div class="container">
         <div class="row justify-content-center mt-5" id="events">
             <div class="col-lg-6  main-content">
                 <div class="entry-wrapper">
                     <div class="entry-single">
                         <div class="entry-header">
-                            <iframe  width="100%" height="400" src="https://player.vimeo.com/video/717629889?h=d603562edc&amp;" style="border:none;border-radius: 15px">
-                            </iframe>
+                            <video
+                                id="my-video2"
+                                class="video-js"
+                                controls
+                                preload="auto"
+                                height="350px"
+                                poster="MY_VIDEO_POSTER.jpg"
+                                data-setup="{}"
+                                style="width: 100%"
+                            >
+                                <source src="/fieldday 2022.mp4.mp4" type="video/mp4" />
+                                <source src="/fieldday 2022.mp4.mp4" type="video/webm" />
+                            </video>
                         </div>
                     </div>
                 </div>
@@ -70,29 +81,6 @@
                 </div>
             </div>
         </div>
-{{--        <div class="row justify-content-center mt-5" id="events">--}}
-{{--            <div class="col-lg-6  main-content">--}}
-{{--                <div class="entry-wrapper">--}}
-{{--                    <div class="entry-single">--}}
-{{--                        <div class="entry-header">--}}
-{{--                            <video--}}
-{{--                            id="my-video"--}}
-{{--                            class="video-js"--}}
-{{--                            controls--}}
-{{--                            preload="auto"--}}
-{{--                            height="350px"--}}
-{{--                            poster="MY_VIDEO_POSTER.jpg"--}}
-{{--                            data-setup="{}"--}}
-{{--                            style="width: 100%"--}}
-{{--                            >--}}
-{{--                            <source src="/terramonte project.mp4" type="video/mp4" />--}}
-{{--                            <source src="/terramonte project.mp4" type="video/webm" />--}}
-{{--                            </video>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
         @foreach($events as $event)
             <div class="row justify-content-center mt-5">
                 <div class="col-lg-12  main-content">
@@ -108,36 +96,36 @@
                                                 </div>
                                             </div>
                                         @endforeach
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7365.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7344.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7343.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC2408.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7337.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="swiper-slide">--}}
-{{--                                            <div class="banner-content-area">--}}
-{{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC2391.jpg');background-size: cover !important;"></div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7365.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7344.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7343.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC2408.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC7337.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
+                                        {{--                                        <div class="swiper-slide">--}}
+                                        {{--                                            <div class="banner-content-area">--}}
+                                        {{--                                                <div class="slider-image slider-image2" style="background-image: url('https://omablear.sirv.com/Images/_DSC2391.jpg');background-size: cover !important;"></div>--}}
+                                        {{--                                            </div>--}}
+                                        {{--                                        </div>--}}
                                     </div>
                                     <div class="swiper-pagination"></div>
                                 </div>
@@ -161,7 +149,7 @@
         @endforeach
     </div>
 
-<!-- about section end -->
+    <!-- about section end -->
 
 @endsection
 @section('scripts')
